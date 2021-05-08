4aa1714984986f0208a62a3fa7e8d5d9992bf2a1
axidex <76962855+axidex@users.noreply.github.com>
2021-05-08 03:07:23 +0300

    Update main.cpp

--- p6/main.cpp
+++ p6/main.cpp
@@ -23,7 +23,7 @@ struct Stadium
     short unsigned int Year;
     unsigned int Watchers;
     short unsigned int NumArena;
-    friend ostream &operator<<(ostream &s, Stadium &el)
+    friend ostream& operator<<(ostream& s, Stadium& el)
     {
         s << el.Name << '\t' << el.Sport << '\t' << el.Year << '\t' << el.Watchers << '\t' << el.NumArena << endl;
         return s;
@@ -34,8 +34,8 @@ template <class T>
 class Element
 {
 public:
-    Element *next;
-    Element *prev;
+    Element* next;
+    Element* prev;
     T info;
 
 public:
@@ -44,19 +44,19 @@ public:
         next = prev = nullptr;
         info = data;
     }
-    Element(Element *Next, Element *Prev, T data)
+    Element(Element* Next, Element* Prev, T data)
     {
         next = Next;
         prev = Prev;
         info = data;
     }
-    Element(const Element &el)
+    Element(const Element& el)
     {
         next = el.next;
         prev = el.prev;
         info = el.info;
     }
-    friend ostream &operator<<(ostream &s, Element<T> &el)
+    friend ostream& operator<<(ostream& s, Element<T>& el)
     {
         s << el.info;
         return s;
@@ -67,8 +67,8 @@ template <class T>
 class LinkedList
 {
 protected:
-    Element<T> *head;
-    Element<T> *tail;
+    Element<T>* head;
+    Element<T>* tail;
     int count;
 
 public:
@@ -77,14 +77,14 @@ public:
         head = tail = nullptr;
         count = 0;
     }
-    virtual Element<T> *pop() = 0;
-    virtual Element<T> *push(T value) = 0;
-    Element<T> &operator[](int index)
+    virtual Element<T>* pop() = 0;
+    virtual Element<T>* push(T value) = 0;
+    Element<T>& operator[](int index)
     {
-        Element<T> *current = head;
+        Element<T>* current = head;
         for (int i = 0;
-             current != nullptr && i < index;
-             current = current->next, i++)
+            current != nullptr && i < index;
+            current = current->next, i++)
             ;
         return *current;
     }
@@ -92,9 +92,9 @@ public:
     {
         return (LinkedList<T>::count == 0);
     }
-    friend ostream &operator<<(ostream &s, LinkedList<T> &el)
+    friend ostream& operator<<(ostream& s, LinkedList<T>& el)
     {
-        Element<T> *current;
+        Element<T>* current;
         for (current = el.head; current != nullptr; current = current->next)
             s << *current;
         return s;
@@ -113,8 +113,8 @@ class FIFO : public LinkedList<T>
 {
 public:
     FIFO<T>() : LinkedList<T>() {}
-    FIFO<T>(T *arr, int len) : LinkedList<T>(arr, len) {}
-    Element<T> *push(T value)
+    FIFO<T>(T* arr, int len) : LinkedList<T>(arr, len) {}
+    Element<T>* push(T value)
     {
         if (LinkedList<T>::head == nullptr) //if(count==0)
         {
@@ -135,15 +135,15 @@ public:
     void insert(T value, int index)
     {
         assert(!(index >= LinkedList<T>::count));
-        int counter;
-        Element<T> *current = LinkedList<T>::head;
+        int counter=0;
+        Element<T>* current = LinkedList<T>::head;
         while (current != nullptr)
         {
-            if (counter == index){break;}
-            current=current->next;
+            if (counter == index) { break; }
+            current = current->next;
             counter++;
         }
-        if (LinkedList<T>::head == nullptr) 
+        if (LinkedList<T>::head == nullptr)
         {
             //пустой список
             LinkedList<T>::head = new Element<T>(value);
@@ -151,37 +151,85 @@ public:
         }
         else if (LinkedList<T>::count == 1)
         {
-            Element<T> *tmp = new Element<T>(value);
+            Element<T>* tmp = new Element<T>(value);
             LinkedList<T>::head = tmp;
             tmp->next = LinkedList<T>::tail;
-            LinkedList<T>::tail->next =nullptr;
+            LinkedList<T>::tail->next = nullptr;
         }
         else if (index == 0)
         {
-            Element<T> *tmp = new Element<T>(value);
-            Element<T> *temp=LinkedList<T>::head;
+            Element<T>* tmp = new Element<T>(value);
+            Element<T>* temp = LinkedList<T>::head;
             LinkedList<T>::head = tmp;
             temp->prev = tmp;
             tmp->next = temp;
         }
-        else if (index == LinkedList<T>::count-1)
+        else if (index == LinkedList<T>::count - 1)
         {
-            Element<T> *tmp = new Element<T>(value);
-            Element<T> *temp=LinkedList<T>::tail->prev;
+            Element<T>* tmp = new Element<T>(value);
+            Element<T>* temp = LinkedList<T>::tail->prev;
             tmp->next = LinkedList<T>::tail;
             tmp->prev = temp;
             temp->next = tmp;
         }
+        else
+        {
+            Element<T>* tmp = new Element<T>(value);
+            tmp->prev = current;
+            tmp->next = current->next;
+            current->next = tmp;
+            current = current->next;
+            current->prev = tmp;
+
+        }
         LinkedList<T>::count++;
         return;
     }
-   
+    void Remove(int index)
+    {
+        assert(!(index >= LinkedList<T>::count));
+        assert(!(LinkedList<T>::count == 0));
+        if (LinkedList<T>::count == 1)
+        {
+            LinkedList<T>::tail = LinkedList<T>::head = nullptr;
+            LinkedList<T>::count = 0;
+            return;
+        }
+        int counter = 0;
+        Element<T>* current = LinkedList<T>::head;
+        while (current != nullptr)
+        {
+            if (counter == index) { break; }
+            current = current->next;
+            counter++;
+        }
+        if (index == 0)
+        {
+            LinkedList<T>::head = LinkedList<T>::head->next;
+            LinkedList<T>::head->prev = nullptr;
+        }
+        else if (index == LinkedList<T>::count - 1)
+        {
+            Element<T>* temp = LinkedList<T>::tail->prev;
+            LinkedList<T>::tail = temp;
+            temp->next = nullptr;
+        }
+        else
+        {
+            Element<T>* tmp = current->prev;
+            current = current->next;
+            tmp->next = current;
+            current->prev = tmp;
+        }
+        LinkedList<T>::count--;
 
-    Element<T> *pop()
+    }
+
+    Element<T>* pop()
     {
         if (LinkedList<T>::head == nullptr)
             return nullptr;
-        Element<T> *res = LinkedList<T>::head;
+        Element<T>* res = LinkedList<T>::head;
         if (LinkedList<T>::head == LinkedList<T>::tail)
             LinkedList<T>::head = LinkedList<T>::tail = nullptr;
         else
@@ -206,13 +254,17 @@ int main()
     Stadium arena3;
     a.push(arena);
     a.push(arena2);
+
     cout << a;
     cout << endl;
     cout << a[0];
     cout << endl
-         << endl;
+        << endl;
     a.insert(arena3, 1);
-    
+    a.insert(arena3, 1);
+    a.Remove(1);
+
+
 
     cout << a << endl;
 
